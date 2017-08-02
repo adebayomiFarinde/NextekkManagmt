1dfa6e329b13ca86d0594fec33c0985d3d94e9bc
AdebayomiFarinde <adebayomi.farinde@yahoo.com>
2017-08-02 10:23:20 +0100

    update

--- Controllers/UserEmployeesController.cs
+++ Controllers/UserEmployeesController.cs
@@ -67,7 +67,7 @@ namespace EmployeeManagement.Controllers
 
         //This loads the list of the user(Employee) from the UserEmployee table 
         // GET: UserEmployees only Admin
-        [MyAuthorize(Roles = "Admin")]
+        [Authorize(Roles = "Admin")]
         public ActionResult Index()
         {
             var userEmployees = db.UserEmployees
@@ -212,40 +212,7 @@ namespace EmployeeManagement.Controllers
             return View(userEmployee);
         }
 
-        //Authorization for different users
-        public class MyAuthorizeAttribute : AuthorizeAttribute
-        {
-            protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
-            {
-                if (!filterContext
-                    .HttpContext
-                    .User
-                    .Identity
-                    .IsAuthenticated)
-                {
-                    // The user is not authenticated
-                    base.HandleUnauthorizedRequest(filterContext);
-                }
-                else if (!this
-                    .Roles.Split(',')
-                    .Any(filterContext
-                    .HttpContext
-                    .User
-                    .IsInRole))
-                {
-                    // The user is not in any of the listed roles => 
-                    // show the unauthorized view
-                    filterContext.Result = new ViewResult
-                    {
-                        ViewName = "~/Views/Shared/Unauthorized.cshtml"
-                    };
-                }
-                else
-                {
-                    base.HandleUnauthorizedRequest(filterContext);
-                }
-            }
-        }
+    
         //Redirecting to this if a User(Employee) has not has not being made active yet by the Admin
         [Authorize]
         public ActionResult Redirect()
